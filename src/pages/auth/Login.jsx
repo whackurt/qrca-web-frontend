3cdb4e692a1c45fa34d2538793b68566a381424d
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-11-30 16:03:26 +0800

    feat: added loading

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserLogin } from '../../services/auth/auth';
+import { ClipLoader } from 'react-spinners';
 
 const Login = () => {
 	const navigate = useNavigate();
 
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+
 	const [errorMsg, setErrorMsg] = useState('');
 	const [loginError, setLoginError] = useState(false);
 
+	const [loading, setLoading] = useState(false);
+
 	const login = async () => {
+		setLoading(true);
+		setLoginError(false);
+		setErrorMsg('');
+
 		await UserLogin({ username: username, password: password }).then((res) => {
 			if (res.data && res.data.hasOwnProperty('token')) {
-				setLoginError(false);
-				setErrorMsg('');
-
 				localStorage.setItem('user_id', res.data.id);
 				localStorage.setItem('token', res.data.token);
 
@@ -25,6 +30,7 @@ const Login = () => {
 				setLoginError(true);
 				setErrorMsg(res.response.data.message);
 			}
+			setLoading(false);
 		});
 	};
 
@@ -37,7 +43,7 @@ const Login = () => {
 				</h1>
 			</div>
 			<div className="flex flex-col items-center">
-				<div className="bg-white rounded-md py-12 px-4 lg:px-10 mx-1 w-[350px] lg:min-w-[600px]">
+				<div className="bg-white rounded-md py-12 px-4 lg:px-10 mx-1 w-[350px] lg:min-w-[450px]">
 					<div className="flex justify-center">
 						<img
 							src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Philippine_National_Police_seal.svg/1466px-Philippine_National_Police_seal.svg.png"
@@ -49,19 +55,21 @@ const Login = () => {
 					<hr className="my-4" />
 					<form action="">
 						<div className="flex flex-col py-2">
-							<p className="font-bold">Username</p>
+							<p className="font-bold text-sm">Username</p>
 							<input
 								onChange={(e) => setUsername(e.target.value)}
 								className="rounded border-slate-300"
 								type="text"
+								name="username"
 							/>
 						</div>
 						<div className="flex flex-col py-2">
-							<p className="font-bold">Password</p>
+							<p className="font-bold text-sm">Password</p>
 							<input
 								onChange={(e) => setPassword(e.target.value)}
 								className="rounded border-slate-300"
 								type="password"
+								name="password"
 							/>
 						</div>
 						<div className="flex flex-col py-2">
@@ -72,10 +80,16 @@ const Login = () => {
 					</form>
 
 					<button
-						onClick={login}
+						onClick={() => login()}
 						className="hover:bg-red-700 bg-primary shadow-md w-full text-white py-2 rounded-md my-4"
 					>
-						Login
+						{loading ? (
+							<span>
+								<ClipLoader size={18} color="#FFFFFF" />{' '}
+							</span>
+						) : (
+							'Login'
+						)}
 					</button>
 				</div>
 			</div>
